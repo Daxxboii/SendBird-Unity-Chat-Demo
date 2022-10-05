c1b01069508e0a8ce10043b514c36d04a4711c84
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2022-10-05 16:33:11 +0530

    Chat Implemented

--- SendBird Demo/Assets/Scripts/SendBirdManager.cs
+++ SendBird Demo/Assets/Scripts/SendBirdManager.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using SendBird;
-using UnityEngine.Networking.Types;
-using Unity.VisualScripting;
 using MyBox;
+using SendBird;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
 
 public class SendBirdManager : MonoBehaviour
 {
@@ -17,20 +16,32 @@ public class SendBirdManager : MonoBehaviour
 
     [Foldout("Private Chat",true)]
     public string UserOnOtherEnd;
+    public bool ViewPrivateChatHistory;
+   
 
     [Foldout("Group Chat",true)]
     public string ClanName;
     public Texture2D ClanImage;
+    public string URL;
     public string Description;
-    public List<SendBird.User> UsersToAddInGroup;
+    public List<SendBird.User> ClanMembers;
+    public bool ViewGroupChatHistory;
+
+    private List<string> ListOfNames;
+
 
 
     private void Awake()
     {
+        ListOfNames = new List<string>();
+        ListOfNames.Add("River Testing");
+        ListOfNames.Add("Daxx");
         InitializeSendBird();
         ConnectToServer();
+        SetHandlers();
     }
 
+    #region Initialize
     private void InitializeSendBird()
     {
         SendBirdClient.SetupUnityDispatcher(gameObject);
@@ -54,21 +65,64 @@ public class SendBirdManager : MonoBehaviour
             }
         });
     }
-    
-    public void CreateToGroup()
+
+    private void SetHandlers()
     {
-        GroupChannel.CreateChannel(UsersToAddInGroup, true, ClanName, ClanImage, Description,"Clan", (GroupChannel groupChannel, SendBirdException e) =>
+        SendBirdClient.ChannelHandler channelHandler = new SendBirdClient.ChannelHandler();
+
+        channelHandler.OnMessageReceived = (BaseChannel baseChannel, BaseMessage baseMessage) => { Debug.Log(baseMessage); };
+        SendBirdClient.AddChannelHandler("UniqueID", channelHandler);
+    }
+
+     #endregion
+
+
+    public void StartGroup()
+    {
+        GroupChannel.CreateChannelWithUserIds(ListOfNames, false, (GroupChannel groupChannel, SendBirdException e) =>
         {
             if (e != null)
             {
                 // Handle error.
+                Debug.LogError(e);
             }
-
-            // A group channel with detailed configuration is successfully created.
-            // By using groupChannel.Url, groupChannel.Data, groupChannel.CustomType, and so on,
-            // you can access the result object from Sendbird server to check your parameter configuration.
+            else
+            {
+                Debug.Log("Group Created");
+            }
             string CustomType = groupChannel.CustomType;
+            string ChanneUrl = groupChannel.Url;
+
+            groupChannel.SendUserMessage("hello", "Message", (UserMessage userMessage, SendBirdException e) =>
+            {
+                Debug.Log(userMessage.Message);
+            });
             
-});
+        });
     }
+
+    public void EnterGroup()
+    {
+        GroupChannel.GetChannel(URL, (GroupChannel groupChannel, SendBirdException e) =>
+        {
+           
+        });
+    }
+
+    public void UpdateGroup()
+    {
+        
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        SendBirdClient.RemoveChannelHandler("Ligma");
+        SendBirdClient.Disconnect(() =>{ });
+    }
+
+
+
+
+
 }
